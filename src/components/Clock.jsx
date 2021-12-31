c30c9ad1bee8a0612a0f9f7538cf764191560c87
Amir Velasquez <66282280+AmirJV1@users.noreply.github.com>
2021-12-31 15:12:44 -0400

    "Fixed Typo on Am - Pm function"

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 const Clock = () => {
 	const [date, setDate] = useState(new Date());
 
@@ -19,33 +18,21 @@ const Clock = () => {
 		<div className="clockContainer">
 			<p>
 				{`${
-					date.getHours() > 12 ? parseInt(date.getHours() - 12) : '0' + date.getHours()
-				}:${date.getMinutes()}:${
+					date.getHours() > 12
+						? parseInt(date.getHours() - 12)
+						: date.getHours() < 10
+						? '0' + date.getHours()
+						: date.getHours()
+				}:${date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes()}:${
 					date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
 				}`}{' '}
-				<span>{date.getHours > 12 ? 'PM' : 'AM'}</span>
+				<span>{date.getHours() > 12 ? 'PM' : 'AM'}</span>
 			</p>
 		</div>
 	);
 };
 
+
 export default Clock;
 
-// const normalizeTime = (time) => (time < 0 ? `0${time}` : time);
 
-// const getCurrentTime = () => {
-// 	let h = date.getHours(); //0-23
-// 	let m = date.getMinutes(); //0-59
-// 	let s = date.getSeconds(); //0 -59
-// 	let sesion = 'AM';
-// 	if (h === 0) h = 12;
-// 	if (h > 12) {
-// 		h = h - 12;
-// 		sesion = 'PM';
-// 	}
-// 	normalizeTime(h);
-// 	normalizeTime(m);
-// 	normalizeTime(s);
-//     console.log(`${h}:${m}:${s}`)
-//     return `${h}:${m}:${s}`
-// };
